469a4019bd2cda5d924396447a19d93addb896c6
kaushikgit0704 <51095770+kaushikgit0704@users.noreply.github.com>
2020-10-04 12:46:54 +0530

    Initial commit of 3.1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dot_net_core3._1_ec2_deploy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
